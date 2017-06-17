787834b0d72304bbd57db140fc251f38bfe6bdc7
Igor Godinho <godinho.igor@gmail.com>
2017-06-17 01:43:00 -0300

    Controle de erros implementado
    Foi implementado o controle de erros para verificar se o arquivo é de fato BMP e se é true color.

--- main.asm
+++ main.asm
@@ -1,7 +1,11 @@
 .data
-header: .space 54	#54 bytes is the standard header size of the bmp image type intended 
-					#		to read
+
+strErrOpenFile: .asciiz "Error opening the file. Are you sure that the name is correct?\n"
+strErrReadFile: .asciiz "Error reading the file. Are you sure that it is a bmp compatible file? \n"
 filename: .asciiz "img.bmp"
+header: .space 540	#54 bytes is the standard header size for a true color bmp image 
+					#		to read
+
 
 
 .text
@@ -22,7 +26,56 @@ main:
 	jal analyseHeader
 		#analyseHeader will analyse the info that was read by readHeader and will put the 
 		#		most relevant info at $s0.
+
+	#encerra a execução do programa
+	jal endProgram
+
 		
+
+analyseHeader:
+	la $t0, header
+
+	#errorControl:
+		#The first 2 bytes are suposed to be (424d)h
+		lb $t1, 0($t0)	#Should be (42)h
+		lb $t2, 1($t0)	#Should be (4D)h
+		bne $t1, 0x42, errReadFile
+		bne $t2, 0x4d, errReadFile
+
+		#The 7th, 8th, 9th and 10th bytes are reserved and suposed to be 0
+		#Since memory is zero-indexed we start by the address of number 6
+		lb $t1, 6($t0)
+		lb $t2, 7($t0)
+		lb $t3, 8($t0)
+		lb $t4, 9($t0)
+		bne $t1, $zero, errReadFile
+		bne $t2, $zero, errReadFile
+		bne $t3, $zero, errReadFile
+		bne $t4, $zero, errReadFile
+
+		#Since we are dealing with a True Color bmp image, we should check if the
+		#	BfOffSetBits field is (54)d as expected.
+		lb $t1, 10($t0)
+		bne $t1, 54, errReadFile
+	#end errorControl
+
+	#At this point we already know that the image is the type we are looking for
+
+	#dataSave:
+	
+
+
+	#end dataSave
+
+
+
+
+	
+	
+	
+
+	jr $ra
+#end analyseHeader
 
 openFile:
 	#sycall for open the file
@@ -30,6 +83,7 @@ openFile:
 	li $a1, 0			# flags (0=read, 1=write)
 	li $a2, 0			# mode = desnecessário
 	syscall				# devolve o descritor (ponteiro) do arquivo em $v0
+	blt $v0, $zero, errOpenFile
 	jr $ra
 #end openFile
 
@@ -44,14 +98,26 @@ readHeader:
 
 errOpenFile:
 	#If the file does not exists this function will be triggered
-
+	la $a0, strErrOpenFile
+	jal printStr
+	jal endProgram
 #end errOpenFile
 
 errReadFile:
 	#If the file information does not come out as it should, this function will be triggered
+	la $a0, strErrReadFile
+	jal printStr
+	jal endProgram
 #end errReadFile
 
 printStr:
+	li $v0, 4
+	syscall
+	jr $ra
 #end printStr
+
+endProgram:
+	li $v0, 10
+	syscall
 
 
