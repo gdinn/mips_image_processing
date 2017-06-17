3c8df1d8cd700177ebc204b7faa63413e729688f
Igor Godinho <godinho.igor@gmail.com>
2017-06-16 21:40:03 -0300

    Funções chave definidas
    Foram definidas e descritas as funções chaves que serão usadas para o procedimento de abrir o arquivo e analisá-lo.

--- main.asm
+++ main.asm
@@ -1,21 +1,57 @@
 .data
-cabecalho: .space 54
-filename: .asciiz "lena.bmp"
+header: .space 54	#54 bytes is the standard header size of the bmp image type intended 
+					#		to read
+filename: .asciiz "img.bmp"
 
 
 .text
-#abre arquivo
-la $a0, filename	# endereço da string com o nome do arquivo
-li $v0, 13			# parametro p chamada de abertura
-li $a1, 0			# flags (0=read, 1=write)
-li $a2, 0			# mode = desnecessário
-syscall				# devolve o descritor (ponteiro) do arquivo em $v0
 
-move $a0, $v0		# mode o descritor para $a0
-li $v0, 14			# parametro de chamada de leitura de arquivo
-la $a1, cabecalho	# endereço para armazenamento dos dados lidos
-li $a2, 54			# tamanho máx de caracteres
-syscall				# devolve o número de caracteres lidos
+main:	
+	la $a0, filename
+	jal openFile
+		#openFile will take $a0 as the name of the file and will take care of
+		#		the rest of the parameters to open the file.
 
-blt $v0, $zero, errOpenArchive
+	la $a1, header 		# Address of the space to copy the file header
+	li $a2, 54			# No. of bytes to read
+	jal readHeader
+		#readHeader will take openFile's return (file descriptor $v0) plus $a1 and $a2.
+
+	la $a0, header 		#$a0 will be the address of the readen data.
+						#It's important that the header follow the 54 byte rule.
+	jal analyseHeader
+		#analyseHeader will analyse the info that was read by readHeader and will put the 
+		#		most relevant info at $s0.
+		
+
+openFile:
+	#sycall for open the file
+	li $v0, 13			# parametro p chamada de abertura
+	li $a1, 0			# flags (0=read, 1=write)
+	li $a2, 0			# mode = desnecessário
+	syscall				# devolve o descritor (ponteiro) do arquivo em $v0
+	jr $ra
+#end openFile
+
+readHeader:
+	move $a0, $v0
+	li $v0, 14			# parametro de chamada de leitura de arquivo	
+	syscall				# devolve o número de caracteres lidos
+	blt $v0, $zero, errReadFile
+	jr $ra
+#end readHeader
+
+
+errOpenFile:
+	#If the file does not exists this function will be triggered
+
+#end errOpenFile
+
+errReadFile:
+	#If the file information does not come out as it should, this function will be triggered
+#end errReadFile
+
+printStr:
+#end printStr
+
 
